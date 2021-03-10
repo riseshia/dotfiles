e0c351d65fed7f8b1a1eade51c902084c2671f90
shia <rise.shia@gmail.com>
2021-03-10 12:42:01 +0900

    Handle deployment path with shell variable

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 set -e
 
-if [ ! -d $HOME/dotfiles ]; then
-  echo "Cloning dotfile repository..."
-  git clone https://github.com/riseshia/dotfiles.git ~/.dotfiles
+DEPLOY_DEST_DIR="$HOME/.dotfiles"
+if [ ! -d $DEPLOY_DEST_DIR ]; then
+  echo "'$DEPLOY_DEST_DIR' dir isn't exist."
+  git clone https://github.com/riseshia/dotfiles.git $DEPLOY_DEST_DIR
+else
+  echo "'$DEPLOY_DEST_DIR' dir exist. Skip repository cloning."
 fi
 
