eeebbb6137619715d647588b428c0a54f7b252c0
shia <rise.shia@gmail.com>
2021-03-10 18:26:45 +0900

    Let OS use bash installed by brew for default shell

--- cookbooks/bash/default.rb
+++ cookbooks/bash/default.rb
@@ -1,1 +1,14 @@
 package "bash"
+
+brew_bash_path = "/usr/local/bin/bash"
+
+# brew install bash and link it to /usr/local/bin/bash. so we need to add it to /etc/shells for chsh.
+execute "add '/usr/local/bin/bash' to /etc/shells" do
+  not_if "grep -q '#{brew_bash_path}' /etc/shells"
+  command "echo \"#{brew_bash_path}\" | sudo tee -a /etc/shells"
+end
+
+execute "change default shell to bash" do
+  not_if "test $SHELL = '#{brew_bash_path}'"
+  command "chsh -s #{brew_bash_path}"
+end
