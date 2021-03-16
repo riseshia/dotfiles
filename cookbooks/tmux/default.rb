596f3eccc9677d0bcd5450840e478f779917fc46
shia <rise.shia@gmail.com>
2021-03-17 08:24:23 +0900

    Add tmux

---
+++ cookbooks/tmux/default.rb
@@ -0,0 +1,3 @@
+package 'tmux'
+
+dotfile '.tmux.conf'
--- roles/darwin/default.rb
+++ roles/darwin/default.rb
@@ -1,4 +1,5 @@
-include_role "base"
+include_role 'base'
 
-include_cookbook "git"
-include_cookbook "bash"
+include_cookbook 'git'
+include_cookbook 'bash'
+include_cookbook 'tmux'
