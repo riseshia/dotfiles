e25c03b5bfc88eef124474006fb82d8a4106dc4f
Shia <rise.shia@gmail.com>
2021-05-10 10:29:11 +0900

    [rbenv] Move ruby function to cookbook

--- cookbooks/functions/default.rb
+++ cookbooks/functions/default.rb
@@ -6,18 +6,3 @@ define :dotfile, source: nil do
     force true
   end
 end
-
-define :ruby, as_global: false do
-  version = params[:name]
-  execute "Install Ruby #{version} via ruby-build" do
-    not_if "rbenv versions | grep -q '#{version}'"
-    command "rbenv install #{version}"
-  end
-
-  if params[:as_global]
-    execute "Set #{version} as global Ruby" do
-      not_if "rbenv global | grep -q '#{version}'"
-      command "rbenv global #{version}"
-    end
-  end
-end
--- cookbooks/rbenv/default.rb
+++ cookbooks/rbenv/default.rb
@@ -1,1 +1,16 @@
 package 'rbenv'
+
+define :ruby, as_global: false do
+  version = params[:name]
+  execute "Install Ruby #{version} via ruby-build" do
+    not_if "rbenv versions | grep -q '#{version}'"
+    command "rbenv install #{version}"
+  end
+
+  if params[:as_global]
+    execute "Set #{version} as global Ruby" do
+      not_if "rbenv global | grep -q '#{version}'"
+      command "rbenv global #{version}"
+    end
+  end
+end
