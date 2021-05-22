46e59e405ebbcefd29ad8ba01a09ecf65a83e50f
Shia <rise.shia@gmail.com>
2021-05-22 22:43:16 +0900

    Fix link_directory not to be recursive

--- cookbooks/functions/default.rb
+++ cookbooks/functions/default.rb
@@ -8,9 +8,13 @@ end
 
 define :link_directory, source: nil do
   source = params[:source] || params[:name]
+  target_dir = File.join(ENV['HOME'], params[:name])
+  src_dir = File.expand_path("../../../#{source}", __FILE__)
 
-  link File.join(ENV['HOME'], params[:name]) do
-    to File.expand_path("../../../#{source}", __FILE__)
+  link target_dir do
+    # Note: link resource don't support -n options for link. so we need to check it manually.
+    only_if %Q|test -h #{target_dir} && [ $(readlink #{target_dir} != #{src_dir} ] && rm #{target_dir}|
+    to src_dir
     user node[:user]
     force true
   end
