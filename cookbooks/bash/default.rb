7ac48a18a10a3e520ad9eb7c6daf1947e7078697
Shia <rise.shia@gmail.com>
2021-05-23 00:02:57 +0900

    [bash] :wave: alacritty

--- cookbooks/bash/default.rb
+++ cookbooks/bash/default.rb
@@ -18,9 +18,10 @@ execute "add 'source .dotbash' to bash_profile" do
   not_if "grep -q '.dotbash' ~/.bash_profile"
   command "echo 'source #{ENV.fetch('HOME')}/.dotfiles/.dotbash' >> ~/.bash_profile"
 end
-copy_file ".alacritty.yml" do
-  source "config/.alacritty.yml"
-end
+
+# copy_file ".alacritty.yml" do
+#   source "config/.alacritty.yml"
+# end
 
 copy_file ".dotfiles/.dotbash" do
   source "config/.dotbash"
