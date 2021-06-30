297313bcaedcf971151fa974f33afe8201ad417f
Shia <rise.shia@gmail.com>
2021-06-30 17:59:43 +0900

    Replace copying .gitconfig file with execute
    I hope this bring more flexiblity to dotfiles...

--- cookbooks/git/default.rb
+++ cookbooks/git/default.rb
@@ -1,8 +1,38 @@
-package 'git'
+package "git"
 
-copy_file '.gitconfig' do
-  source 'config/.gitconfig'
+copy_file ".gitignore" do
+  source "config/.gitignore"
 end
-copy_file '.gitignore' do
-  source 'config/.gitignore'
+
+gitconfig = {
+  core: {
+    excludesFile: "~/.gitignore",
+    editor: "vim",
+    quotepath: "off",
+  },
+  color: {
+    ui: "true",
+  },
+  push: {
+    default: "simple",
+  },
+  pull: {
+    rebase: "true",
+  },
+  submodule: {
+    recurse: "true",
+  },
+  init: {
+    defaultBranch: "main",
+  },
+}
+
+gitconfig.each do |namespace, kv|
+  kv.each do |key, value|
+    full_key = "#{namespace}.#{key}"
+
+    execute "git config --global #{full_key} #{value}" do
+      not_if "[ $(git config --global #{full_key}) = #{value} ]"
+    end
+  end
 end
