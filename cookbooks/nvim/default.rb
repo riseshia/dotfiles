fdcc3cfe9f9b2e934ae01db80a69c4607b7759bb
Shia <rise.shia@gmail.com>
2022-06-27 21:38:13 +0900

    Use packer as plugin manager

--- cookbooks/nvim/default.rb
+++ cookbooks/nvim/default.rb
@@ -3,3 +3,15 @@ package "neovim"
 link_directory ".config/nvim" do
   source "config/nvim"
 end
+
+packer_repository_url = "https://github.com/wbthomason/packer.nvim"
+nvim_config_dir = "~/.local/share/nvim/site/pack/packer/start/packer.nvim"
+
+execute "Install packer for vim" do
+  not_if "test -d #{nvim_config_dir}"
+  command "git clone --depth 1 #{packer_repository_url} #{nvim_config_dir}"
+end
+
+link_directory ".config/nvim/colors" do
+  source "config/vim-colors"
+end
