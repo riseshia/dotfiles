130866919539d6eb204ce775ac202af0aa4db597
Shia <rise.shia@gmail.com>
2022-10-16 16:32:15 +0900

    Add comment

--- config/nvim/lua/lsp-config.lua
+++ config/nvim/lua/lsp-config.lua
@@ -22,6 +22,7 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  -- KK するとホーバーしたやつの中にカーソルが移る
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
   vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
   -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
