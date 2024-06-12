3dee20a34b14d9be2146a9932b7228a6d8f1939a
Shia <rise.shia@gmail.com>
2024-06-12 22:54:42 +0900

    Use clone_github_repo to install packer

--- cookbooks/nvim/default.rb
+++ cookbooks/nvim/default.rb
@@ -4,12 +4,9 @@ link_directory ".config/nvim" do
   source "config/nvim"
 end
 
-packer_repository_url = "https://github.com/wbthomason/packer.nvim"
-nvim_config_dir = "~/.local/share/nvim/site/pack/packer/start/packer.nvim"
-
-execute "Install packer for vim" do
-  not_if "test -d #{nvim_config_dir}"
-  command "git clone --depth 1 #{packer_repository_url} #{nvim_config_dir}"
+clone_github_repo "packer" do
+  repository "wbthomason/packer.nvim"
+  clone_path "#{ENV['HOME']}/.local/share/nvim/site/pack/packer/start/packer.nvim"
 end
 
 link_directory ".config/nvim/colors" do
